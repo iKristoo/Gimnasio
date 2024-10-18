4ec77b5dd6f18ae89823ca30be6fefc2e44b2d88
iKristoo <ikristokaynee@gmail.com>
2024-10-18 19:24:42 -0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,287 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox8 = New TextBox()
+        ComboBox1 = New ComboBox()
+        ComboBox3 = New ComboBox()
+        CheckBox1 = New CheckBox()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Arial", 25F, FontStyle.Bold)
+        Label1.Location = New Point(181, 14)
+        Label1.Margin = New Padding(5, 0, 5, 0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(272, 40)
+        Label1.TabIndex = 0
+        Label1.Text = "Cambio de Plan"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Arial", 15F)
+        Label2.Location = New Point(19, 80)
+        Label2.Margin = New Padding(5, 0, 5, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(144, 23)
+        Label2.TabIndex = 1
+        Label2.Text = "Codigo de Plan"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Arial", 15F)
+        Label3.Location = New Point(19, 127)
+        Label3.Margin = New Padding(5, 0, 5, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(43, 23)
+        Label3.TabIndex = 2
+        Label3.Text = "DNI"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Arial", 15F)
+        Label4.Location = New Point(19, 175)
+        Label4.Margin = New Padding(5, 0, 5, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(59, 23)
+        Label4.TabIndex = 3
+        Label4.Text = "Turno"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Arial", 15F)
+        Label5.Location = New Point(19, 222)
+        Label5.Margin = New Padding(5, 0, 5, 0)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(74, 23)
+        Label5.TabIndex = 4
+        Label5.Text = "Horario"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Arial", 15F)
+        Label6.Location = New Point(19, 270)
+        Label6.Margin = New Padding(5, 0, 5, 0)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(87, 23)
+        Label6.TabIndex = 5
+        Label6.Text = "Duración"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Arial", 15F)
+        Label7.Location = New Point(19, 318)
+        Label7.Margin = New Padding(5, 0, 5, 0)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(142, 23)
+        Label7.TabIndex = 6
+        Label7.Text = "Con/Sín Coach"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Arial", 15F)
+        Label8.Location = New Point(19, 365)
+        Label8.Margin = New Padding(5, 0, 5, 0)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(56, 23)
+        Label8.TabIndex = 7
+        Label8.Text = "Pago"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Arial", 15F)
+        Label9.Location = New Point(19, 413)
+        Label9.Margin = New Padding(5, 0, 5, 0)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(78, 23)
+        Label9.TabIndex = 8
+        Label9.Text = "Importe"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Arial", 15F)
+        TextBox1.Location = New Point(390, 75)
+        TextBox1.Margin = New Padding(5, 5, 5, 5)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(226, 30)
+        TextBox1.TabIndex = 9
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Font = New Font("Arial", 15F)
+        TextBox2.Location = New Point(390, 123)
+        TextBox2.Margin = New Padding(5, 5, 5, 5)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(226, 30)
+        TextBox2.TabIndex = 10
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Font = New Font("Arial", 15F)
+        TextBox4.Location = New Point(390, 218)
+        TextBox4.Margin = New Padding(5, 5, 5, 5)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(226, 30)
+        TextBox4.TabIndex = 12
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Font = New Font("Arial", 15F)
+        TextBox5.Location = New Point(390, 265)
+        TextBox5.Margin = New Padding(5, 5, 5, 5)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(226, 30)
+        TextBox5.TabIndex = 13
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Font = New Font("Arial", 15F)
+        TextBox8.Location = New Point(390, 408)
+        TextBox8.Margin = New Padding(5, 5, 5, 5)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(226, 30)
+        TextBox8.TabIndex = 16
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Items.AddRange(New Object() {"Semanal", "Mensual", "Anual"})
+        ComboBox1.Location = New Point(390, 362)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(226, 31)
+        ComboBox1.TabIndex = 17
+        ' 
+        ' ComboBox3
+        ' 
+        ComboBox3.FormattingEnabled = True
+        ComboBox3.Items.AddRange(New Object() {"Mañana", "Tarde", "Noche"})
+        ComboBox3.Location = New Point(390, 172)
+        ComboBox3.Name = "ComboBox3"
+        ComboBox3.Size = New Size(226, 31)
+        ComboBox3.TabIndex = 19
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(500, 327)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(15, 14)
+        CheckBox1.TabIndex = 20
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(19, 492)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(129, 32)
+        Button2.TabIndex = 22
+        Button2.Text = "Cancelar"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(431, 492)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(185, 32)
+        Button3.TabIndex = 23
+        Button3.Text = "Guardar Cambios"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(11F, 23F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(635, 554)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(CheckBox1)
+        Controls.Add(ComboBox3)
+        Controls.Add(ComboBox1)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Font = New Font("Arial", 15F)
+        Margin = New Padding(5, 5, 5, 5)
+        Name = "Form1"
+        Text = "Cambio de Plan"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents ComboBox3 As ComboBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+
+End Class
